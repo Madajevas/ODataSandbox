46ecd182dd6da6da5228135645ebd605ec9859cb
Tomas Madajevas <tomas.madajevas@live.com>
2023-09-08 21:59:49 +0300

    [rebase] now works

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,11 +1,20 @@
 using Microsoft.AspNetCore.OData;
+using Microsoft.AspNetCore.OData.Batch;
+using Microsoft.OData.Edm;
+using Microsoft.OData.ModelBuilder;
+
+using WebAPI;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
+var oDataBuilder = new ODataConventionModelBuilder();
+oDataBuilder.EntitySet<WeatherForecast>("WeatherForecast");
+IEdmModel model = oDataBuilder.GetEdmModel();
+
+// Add services to the container.
 builder.Services.AddControllers()
-    .AddOData(options => options.Select());
+    .AddOData(options => options.AddRouteComponents("odata", model, new DefaultODataBatchHandler()).Filter().Select().Expand().OrderBy().SetMaxTop(25));
 
 
 var app = builder.Build();
@@ -16,6 +25,10 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+app.UseODataBatching();
+app.UseRouting();
+
 app.MapControllers();
+
 
 app.Run();
